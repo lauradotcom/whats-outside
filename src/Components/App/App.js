062135375f4ab52653c6e26d56b808b57bd55b19
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2021-07-19 20:47:38 -0700

    Add headings to main app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>What's Outside?</h1>
+      <h2>Weather, Pollen and Air Quality Forecast</h2>
       <SearchBar />
     </div>
   );
